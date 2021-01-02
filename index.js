4a6414f2f01d196ed94811e2049bac28760096c2
Chiquicalris <38810865+chiquicalris-dev@users.noreply.github.com>
2021-01-02 12:25:14 +0100

    ✨ · ¡Espero que te sirva de ayuda!

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const { prefix, token } = require('./config.json');
+
+
+client.on('ready', () => {
+   console.log(`Estoy listo!`);
+});
+
+client.on('message', (message) => {
+    if(message.content.startsWith('shard')) {
+      message.channel.send(`Mi shard en este servidor es: **${shard.id}**`);
+    }
+
+});
+
+client.login(client.config.token).catch(console.error);
