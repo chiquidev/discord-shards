63bd522b2e97c5381cd8575b7b2054ea0b4daabf
Chiquicalris <38810865+chiquicalris-dev@users.noreply.github.com>
2021-01-02 13:46:01 +0100

    ✨ uyuy

--- shard.js
+++ shard.js
@@ -4,4 +4,10 @@ const Manager = new Discord.ShardingManager('./index.js');
 
 // SHARD
 Manager.on('shardCreate', shard => console.log(`[LANZADO] ${shard.id} ejecutado correctamente.`));
-Manager.spawn(2); // Aquí, debemos seleccionar el número de shards que deseamos ejecutar en nuestro bot. (Si ponemos auto, el manager seleccionará automáticamente el número de shards necesarios).
+Manager.spawn(2); 
+/*
+Aquí, debemos seleccionar el número de shards que 
+deseamos ejecutar en nuestro bot. (Si ponemos auto, 
+el manager seleccionará automáticamente el número de
+shards necesarios).
+*/
