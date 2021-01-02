a58f4e32b3974de37171ffb42d2b8d0da6212b87
Chiquicalris <38810865+chiquicalris-dev@users.noreply.github.com>
2021-01-02 13:12:04 +0100

    ✨ Es añadido una función para obtener el ping del shard

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const { prefix, token } = require('./config.json');
 
-
 client.on('ready', () => {
-   console.log(`Estoy listo!`);
+    console.log(`Estoy listo!`)
 });
+
 
 client.on('message', (message) => {
     const Discord = require("discord.js");
     if(message.content.startsWith('shard')) {
-      message.channel.send(`Mi shard en este servidor es: **${message.guild.shardID}**`);
+      message.channel.send(`Mi shard en este servidor es: **${message.guild.shardID}** y tiene un ping de **${message.guild.shard.ping}**ms`);
     }
 
+
+    client.user.setPresence({
+        game: 'online',
+        activity: {
+            name: `✨ #${message.guild.shard.status}`,
+            type: 'WATCHING'
+        }
+    })
 });
 
 client.login(config.token).catch(console.error);
