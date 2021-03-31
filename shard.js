cb367ffae7f0b1773a253f91b242729f2818e6f8
Chiquicalris <chiquicalris@gmail.com>
2021-03-31 10:02:57 +0200

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,9 +1,13 @@
+// IMPORTACIONES
+const Discord = require("discord.js");;
+const Manager = new Discord.ShardingManager('./index.js');
+
+// SHARD
 Manager.on('shardCreate', shard => console.log(`[LANZADO] ${shard.id} ejecutado correctamente.`));
-Manager.spawn(2); 
+Manager.spawn(2); // Aquí, debemos seleccionar el número de shards que deseamos ejecutar en nuestro bot. (Si ponemos auto, el manager seleccionará automáticamente el número de shards necesarios). 
 /*
 Aquí, debemos seleccionar el número de shards que 
 deseamos ejecutar en nuestro bot. (Si ponemos auto, 
 el manager seleccionará automáticamente el número de
 shards necesarios).
 */
- 
